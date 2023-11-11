b00ef185431a04ca8c53a36fd310a251dd78c5b8
joaquin <joaquineaceituno@gmail.com>
2023-11-11 16:14:32 -0300

    funcion para agregar iconos de navegacion

--- navigation.js
+++ navigation.js
@@ -6,3 +6,31 @@ import inicio from "./pantallas/inicio"
 import boton from "./pantallas/boton";
 import mapa from "./pantallas/mapa";
 import {Ionicons} from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();//variable para los iconos
+
+function TabGroup() { 
+    return (
+    <Tab.Navigator
+        screenOptions={({route}) => ({
+        tabBarShowLabel: false,
+        tabBarIcon: ({color, focused, size}) => {
+            let iconName;
+            if (route.name === "inicio") {
+            iconName = focused ? "home" : "home-outline";//icono para el inicio
+            } else if (route.name === "mapa") {
+            iconName = focused ? "map" : "map-outline";//icono para el mapa
+            } else if (route.name === "boton") {
+            iconName = focused ? "circle" : "circle-o";//icono para el boton
+            }
+            return <Ionicons name={iconName} color={color} size={size} />
+        },
+        })}
+        >
+        <Tab.Screen name="inicio" component={Tabinicio} style={style.inicio}/> 
+        <Tab.Screen name="boton" component={Tabboton} style={style.boton}/>
+        <Tab.Screen name="mapa" component={Tabmapa} style={style.inicio}/>
+        
+        </Tab.Navigator> //Iconos de inicio, boton y mapa
+    )
+    }
