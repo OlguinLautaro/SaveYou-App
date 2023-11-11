4c9965930ee256c08e14eabf660a949d993cb311
joaquin <joaquineaceituno@gmail.com>
2023-11-11 16:01:55 -0300

    se importan las dependencias

---
+++ navigation.js
@@ -0,0 +1,8 @@
+import react from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { View, Text, StyleSheet } from "react-native";
+import inicio from "./pantallas/inicio"
+import boton from "./pantallas/boton";
+import mapa from "./pantallas/mapa";
+import {Ionicons} from "@expo/vector-icons";
